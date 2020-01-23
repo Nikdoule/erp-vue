342da57da1e1d56a1c03fb9af3fb9ec8666ddaba
Nikdoule <yyperite@hotmail.fr>
2020-01-23 19:41:47 +0100

    final

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,12 +19,12 @@ class UsersTableSeeder extends Seeder
         DB::table('role_user')->truncate();
 
         $admin = User::create([
-            'name' => 'administrador',
+            'name' => 'Admin',
             'email' => 'admin@admin.com',
             'password' => Hash::make('123456789')
         ]);
         $manager = User::create([
-            'name' => 'copin',
+            'name' => 'Manager',
             'email' => 'manager@manager.com',
             'password' => Hash::make('123456789')
         ]);
